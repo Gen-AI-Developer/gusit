c9878820aaf9f179bcfa058db1b840c894ccbd91
SafdarAliShah <programmersafdar@live.com>
2024-11-08 22:41:15 +0500

    Project: Structural Updated: added Pages

---
+++ app/components/sections/Banner.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-[#2E577D] text-1xl  text-white sm:hidden lg:flex lg:justify-between">
+      <div className="flex gap-4 py-3 px-3 font-bold">
+        <div>PSH : +92 91-5825707</div>
+        <div>D.I.KHAN : +92 966-714007</div>
+        <div>info@qurtuba.edu.pk</div>
+      </div>
+      <div>
+        <button className="hover:bg-white hover:text-[#2E577D] h-full px-4 font-bold">
+          Online Admission
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ app/components/sections/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <section className="bg-white">
+      <nav className="sticky top-0 flex items-center justify-between">
+        <div className="text-black font-bold text-4xl">QUSIT</div>
+        <div className=" flex gap-5">
+          <Link className="" href={"/"}>
+            Home
+          </Link>
+          <Link href={"/aboutus"}>About</Link>
+          <Link href={"/admissions"}>Admissions</Link>
+          <Link href={"/departments"}>Departments</Link>
+          <Link href={"/faculties"}>Faculties</Link>
+          <Link href={"/oric"}>ORIC</Link>
+          <Link href={"/reseachjournals"}>Reseach Journals</Link>
+          <Link href={"/qec"}>QEC</Link>
+          <Link href={"/portal-mis"}>Portal/MIS</Link>
+          <Link href={"/convocations"}>Convocations</Link>
+        </div>
+      </nav>
+    </section>
+  );
+};
+
+export default Navbar;
